9e40ef38a936dc54d0bbf0a11a717a2f0b8d8028
Lucky <asfaqeh@gmail.com>
2024-12-24 00:16:27 +0530

    Library installation

--- android/app/src/main/java/com/testapp/MainActivity.kt
+++ android/app/src/main/java/com/testapp/MainActivity.kt
@@ -1,4 +1,6 @@
 package com.testapp
+import android.os.Bundle;
+
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -7,6 +9,7 @@ import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
 
+  
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
@@ -19,4 +22,7 @@ class MainActivity : ReactActivity() {
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)
+  }
 }
